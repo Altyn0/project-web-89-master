dfc716b03e7b65908e503fc2638c04d40ebb5e10
Altyn0 <96158393+Altyn0@users.noreply.github.com>
2023-11-09 00:25:38 +0700

    bug data

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
 /** @format */
-
-import { Home } from 'iconsax-react';
 import './App.css';
-import HomeScreen from './screens/HomeScreen';
-import ProfileScreen from './screens/ProfileScreen';
-import AddTask from './components/AddTask';
-import TasksComponent from './components/TasksComponent';
-import { useState } from 'react';
-import ScoreComponent from './components/ScoreComponent';
-import { ButtonComponent } from './components/ButtonComponent';
-import { users } from './data/users';
-import ListUserItem from './components/ListUserItem';
+import { List } from 'antd';
+import { posts } from './data/posts';
 
 function App() {
-	const usersWithCompany = users.filter((element) => element.company)
-	const handleCheckEmail = (id) => {
-		const user = users.find(element => element.id===id);
-		user &&	alert(user.email);
-		
-	};
 	return (
 	<div className='container mt-4'>
-		<div className='col-8 offset-2'>
-			{usersWithCompany.map((item,index) =>  (<ListUserItem onCheckEmail={() => handleCheckEmail(item.id)} user={item} color={index % 2 ? 'coral':'green'} />  ))}
+		<div className='row'>      
+			<div className='col-8 offset-2'>
+			<List 
+			 dataSource={posts}
+			 renderItem={(item, _index) => (<List.Item key={item.id}>
+				<List.Item.Meta title={item.title} description={item.body} />  
+				</List.Item>
+				)}  
+			/>
+			</div>
 		</div>
 	</div>
-	
 	);
 }
 
